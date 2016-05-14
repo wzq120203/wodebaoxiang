78f87e7a7f7ba51869c7db71234b4933451dd62d
wzq120203 <898378241@qq.com>
2016-05-14 19:26:58 +0800

    更新博客

--- js/1.js
+++ js/1.js
@@ -17,14 +17,32 @@ window.onload=function(){
 
 
 
-	var change=document.querySelector("#change")
-	var prev=document.querySelector('#prev')
-	var next=document.querySelector('#next')
-	var box=document.querySelector('#box')
+	var change=document.getElementById("change")
+	var prev=document.getElementById('prev')
+	var next=document.getElementById('next')
+	var box=document.getElementById('box')
 	var picture=document.querySelector('.picture')
 	var inputs=document.querySelectorAll('#ind span')
-	var wrap=document.querySelector('#box .wrap')
-	var divs=document.querySelectorAll('#box .wrap ul li')
+	var wrap=document.querySelector('.wrap ul')
+	var divs=document.querySelectorAll('.wrap ul li')
+	var oLi=document.getElementById('li');
+	var oImgs=box.getElementsByTagName('img');
+
+
+
+
+for (var i = 0; i < oImgs.length; i++) {
+	oImgs[i].onclick=function(){
+		this.src = this.src.replace("png","gif");//点击宝箱后更换成gif图
+		
+			setTimeout(function(){
+			for (var i = 0; i < oImgs.length; i++) {
+				oImgs[i].src=oImgs[i].src.replace("gif","png");
+				}
+			},2000);
+		
+	}
+}
 
 
 
@@ -45,7 +63,7 @@ window.onload=function(){
 			index=0;
 			
 		} 
-			wrap.style.left=-index*box.offsetWidth+'px';	
+			wrap.style.left=-index*oLi.offsetWidth+'px';	
 			for (var i=0;i< inputs.length;i++) {
 					inputs[i].className="";
 				}
@@ -58,7 +76,7 @@ window.onload=function(){
 		timer=setInterval(function () {
 			nextFn();
 
-		},300000);
+		},30000);
 	};
 	clearTimer();
 //右移动----------封装函数
@@ -70,13 +88,13 @@ function  prevFn() {
 		if (index<0) {
 			index=divs.length-1;
 		}
-		wrap.style.left=-index*box.offsetWidth+'px';
+		wrap.style.left=-index*oLi.offsetWidth+'px';
 	}
 	for (var i = 0; i < inputs.length; i++) {
 		inputs[i].index=i
 		inputs[i].onclick=function () {
 			index=this.index;
-			wrap.style.left=-this.index*box.offsetWidth+'px';			
+			wrap.style.left=-this.index*oLi.offsetWidth+'px';			
 			clearTimer();
 			for (var i=0;i< inputs.length;i++) {
 					inputs[i].className="";
@@ -89,20 +107,20 @@ function  prevFn() {
 
 picture.onmouseenter=function(){
 	
-	prev.style.opacity='1';
+	// prev.style.opacity='1';
 	next.style.opacity='1';
 }
 picture.onmouseleave=function () {
 	
-	prev.style.opacity='0';
+	// prev.style.opacity='0';
 	next.style.opacity='0';
 	
 }
-wrap.onmouseover=function () {
+wrap.onmouseenter=function () {
 	clearInterval(timer)
 
 }
-wrap.onmouseout=function () {
+wrap.onmouseleave=function () {
 	clearTimer();
 	
 }
